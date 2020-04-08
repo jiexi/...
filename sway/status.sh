769621fcf374d9e3b0868a2de06dc359ef16b5c2
Jiexi Luan <jiexiluan@gmail.com>
2020-04-08 11:57:16 -0700

    Fix status time

--- sway/status.sh
+++ sway/status.sh
@@ -5,7 +5,7 @@
 # The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
 # like 2018-10-06 and the time (e.g., 14:01). Check `man date` on how to format
 # time and date.
-date_formatted=$(date "+%a %F %H:%M")
+date_formatted=$(date "+%a, %b %d - %I:%M%p")
 
 # "upower --enumerate | grep 'BAT'" gets the battery name (e.g.,
 # "/org/freedesktop/UPower/devices/battery_BAT0") from all power devices.
@@ -20,22 +20,9 @@ grep 'BAT') |\
 egrep "state|percentage" |\
 awk '{print $2}')
 
-# "amixer -M" gets the mapped volume for evaluating the percentage which
-# is more natural to the human ear according to "man amixer".
-# Column number 4 contains the current volume percentage in brackets, e.g.,
-# "[36%]". Column number 6 is "[off]" or "[on]" depending on whether sound
-# is muted or not.
-# "tr -d []" removes brackets around the volume.
-# Adapted from https://bbs.archlinux.org/viewtopic.php?id=89648
-audio_volume=$(amixer -M get Master |\
-awk '/Mono.+/ {print $6=="[off]" ?\
-$4" 🔇": \
-$4" 🔉"}' |\
-tr -d [])
-
 # Additional emojis and characters for the status bar:
 # Electricity: ⚡ ↯ ⭍ 🔌
 # Audio: 🔈 🔊 🎧 🎶 🎵 🎤
 # Separators: \| ❘ ❙ ❚
 # Misc: 🐧 💎 💻 💡 ⭐ 📁 ↑ ↓ ✉ ✅ ❎
-echo $audio_volume $battery_info 🔋 $date_formatted
+echo $battery_info 🔋 $date_formatted
