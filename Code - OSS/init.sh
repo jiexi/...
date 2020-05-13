a01d0fefb644855661e086258dd4e87cd396f3b4
Jiexi Luan <jiexiluan@gmail.com>
2020-05-13 10:49:54 -0700

    Migrate from OSS Code to VSCode

--- Code - OSS/init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-code --install-extension skyapps.fish-vscode
-code --install-extension vscodevim.vim
---
+++ Code/init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+code --install-extension johnpapa.vscode-peacock
+code --install-extension JuanBlanco.solidity
+code --install-extension karigari.chat
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vsliveshare.vsliveshare-audio
+code --install-extension ms-vsliveshare.vsliveshare-pack
+code --install-extension skyapps.fish-vscode
+code --install-extension vscodevim.vim
