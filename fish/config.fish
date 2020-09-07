210097a0d5ac5f09c48ac0606812ef560d161e03
Jiexi Luan <jiexiluan@gmail.com>
2020-09-07 02:49:16 -0700

    Cleanup commented fish alias

--- fish/config.fish
+++ fish/config.fish
@@ -38,24 +38,9 @@ end
 
 
 # TODO(JL): clean these up
-# git
-# alias gp  "git pull"
-# alias gpp "git pullpush"
-# alias gs  "git st"
-
-# utilities
-# alias f "vifm ."
-# alias t "tmux"
-# alias v "nvim"
-# alias todol "todo list --sort priority,-due"
-
 # clipboard
 # alias cbc "clipboard copy"
 # alias cbp "clipboard paste"
-
-# docker
-# alias dc   "docker-compose"
-# alias dcub "docker-compose up --build"
 
 # !! and !$ substitution
 function bind_bang
