d7f8e7f4226945b995afda5f0aa470a9430d27fb
Jiexi Luan <jiexiluan@gmail.com>
2023-05-23 14:41:07 -0700

    Update vscode plugins

--- Code/init.sh
+++ Code/init.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 code --install-extension johnpapa.vscode-peacock
-code --install-extension JuanBlanco.solidity
-code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension ms-vsliveshare.vsliveshare-audio
-code --install-extension ms-vsliveshare.vsliveshare-pack
 code --install-extension skyapps.fish-vscode
 code --install-extension vscodevim.vim
